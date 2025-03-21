8012fdb838b8ba528149a51427586ac6a2ce6de1
gideok Kim <ikid4127@naver.com>
2025-03-21 10:00:23 +0900

    Fix GitHub Pages deployment configuration

--- calculator/webpack.config.js
+++ calculator/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
-        publicPath: '/calculator/'
+        publicPath: process.env.NODE_ENV === 'production' ? '/calculator/' : '/'
     },
     module: {
         rules: [
